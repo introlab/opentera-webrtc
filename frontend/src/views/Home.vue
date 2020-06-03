75c8ecb801c004462471973a8235188e2fb9bd37
Dominic Létourneau <doumdi@gmail.com>
2020-06-03 17:02:19 -0400

    Testing vue-webrtc

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -14,28 +14,42 @@
     <div class="col-md-12">
       <button type="button" class="btn btn-danger" @click="onStop">Stop Camera</button>
       <button type="button" class="btn btn-success" @click="onStart">Start Camera</button>
+      <button type="button" class="btn btn-success" @click="onJoin">Join</button>
+    </div>
+    <div>
+      <vue-webrtc ref="webrtc" width="100%" roomId="roomId" socketURL="https://localhost:9001/" :enableLogs="logEnabled" @>
+      </vue-webrtc>
     </div>
   </div>
-
+<!--
     <VideoView
       ref="webcam"
-      selectFirstDevice="true"
       :name="name"
       :deviceId="deviceId"
       @cameras="camera_list"
       @camera-change="onCameraChange"
+      @started="onCameraStarted"
     />
+-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import VideoView from '@/components/VideoView.vue'
+// import VideoView from '@/components/VideoView.vue'
+import WebRTC from 'vue-webrtc'
+import Vue from 'vue'
+// ISSUE 5: https://github.com/westonsoftware/vue-webrtc/issues/5
+import * as io from 'socket.io-client'
+window.io = io
+
+Vue.use(WebRTC)
+// Vue.component(WebRTC.name, WebRTC)
 
 export default {
   name: 'Home',
   components: {
-    VideoView
+    // VideoView
   },
   data () {
     return {
@@ -43,7 +57,8 @@ export default {
       camera: null,
       deviceId: null,
       name: null,
-      devices: []
+      devices: [],
+      logEnabled: true
     }
   },
   methods: {
@@ -61,10 +76,17 @@ export default {
     onStart () {
       this.$refs.webcam.start()
     },
+    onCameraStarted () {
+      console.log('Camera Started')
+    },
     onCameraChange (deviceId) {
       this.deviceId = deviceId
       this.camera = deviceId
       console.log('On Camera Change Event', deviceId)
+    },
+    onJoin () {
+      console.log('On join')
+      this.$refs.webrtc.join()
     }
   }
 }
