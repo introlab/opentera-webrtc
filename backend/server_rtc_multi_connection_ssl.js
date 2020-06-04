ad773219dff85268eb16a297bd47b6d058279911
Dominic Létourneau <doumdi@gmail.com>
2020-06-04 15:55:15 -0400

    Testing rtcmulticonnection

---
+++ backend/server_rtc_multi_connection_ssl.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var https = require("https");
+var fs = require("fs");
+var io = require("socket.io");
+var RTCMultiConnectionServer = require('rtcmulticonnection-server');
+
+var frontendRouter = require('./routes/frontend');
+var apiIndexRouter = require('./routes/api/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'static')));
+
+//Setup routes
+app.use('/', frontendRouter);
+app.use('/api/', apiIndexRouter);
+
+//Setup https webserver
+var webServer = https.createServer({
+    key:  fs.readFileSync("ssl/key.pem"),
+    cert: fs.readFileSync("ssl/cert.pem")
+  }, app).listen(9001);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = io.listen(webServer, {"log level":1});
+
+//Setup iceServers
+var appIceServers = [
+  {
+    "url":"stun:telesante.3it.usherbrooke.ca:3478"
+  },
+  {
+    "url":"turn:telesante.3it.usherbrooke.ca:50000", 
+    "username":"teraplus",
+    "credential":"teraplus"
+  },
+  {
+    "url":"turn:telesante.3it.usherbrooke.ca:50000?transport=tcp",
+    "username":"teraplus",
+    "credential":"teraplus"
+  }
+];
+
+RTCMultiConnectionServer.appIceServers = appIceServers
+console.log(RTCMultiConnectionServer.appIceServers)
+
+var config = {
+    "enableLogs": "true"
+}
+
+
+// Listen to socket
+socketServer.on('connection', function(socket) {
+    RTCMultiConnectionServer.addSocket(socket, config);
+});
+
+
+
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   outputDir: '../backend/static',
   configureWebpack: {
-    
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
+      }
+    }
   }
 
 }
