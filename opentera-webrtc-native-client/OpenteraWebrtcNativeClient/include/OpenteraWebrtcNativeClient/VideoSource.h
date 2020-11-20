0aba62030d469d13f0c5a42cb267a6a794bb2de5
Cédric Godin <cedric.godin@usherbrooke.ca>
2020-11-20 09:38:35 -0500

    Fix include guard symbol name in new VideoSource.h

--- opentera-webrtc-native-client/OpenteraWebrtcNativeClient/include/OpenteraWebrtcNativeClient/VideoSource.h
+++ opentera-webrtc-native-client/OpenteraWebrtcNativeClient/include/OpenteraWebrtcNativeClient/VideoSource.h
@@ -1,5 +1,5 @@
-#ifndef OPENTERA_WEBRTC_NATIVE_CLIENT_REF_COUNTED_VIDEO_SOURCE_H
-#define OPENTERA_WEBRTC_NATIVE_CLIENT_REF_COUNTED_VIDEO_SOURCE_H
+#ifndef OPENTERA_WEBRTC_NATIVE_CLIENT_VIDEO_SOURCE_H
+#define OPENTERA_WEBRTC_NATIVE_CLIENT_VIDEO_SOURCE_H
 
 #include <media/base/adapted_video_track_source.h>
 #include <rtc_base/ref_counted_object.h>
