b6c10a5ea8d42d6a829531b12ecaae235cb6b1bb
Marc-Antoine Maheux <Marc-Antoine.Maheux@USherbrooke.ca>
2021-03-10 15:21:02 -0500

    Update webrtc

--- opentera-webrtc-native-client/3rdParty/webrtc_native/generate_ninja_files.bash
+++ opentera-webrtc-native-client/3rdParty/webrtc_native/generate_ninja_files.bash
@@ -23,6 +23,6 @@ case "$a" in
 	;;
 esac
 
-common_flags="is_clang=false is_component_build=false treat_warnings_as_errors=false fatal_linker_warnings=false use_gio=false use_rtti=true use_custom_libcxx=false rtc_enable_protobuf=false rtc_include_tests=false rtc_use_h264=true use_sysroot=false proprietary_codecs=true host_cpu=\"$a\" current_cpu=\"$a\" target_cpu=\"$a\"$extras"
+common_flags="rtc_use_pipewire=false is_clang=false is_component_build=false treat_warnings_as_errors=false fatal_linker_warnings=false use_gio=false use_rtti=true use_custom_libcxx=false rtc_enable_protobuf=false rtc_include_tests=false rtc_use_h264=true use_sysroot=false proprietary_codecs=true host_cpu=\"$a\" current_cpu=\"$a\" target_cpu=\"$a\"$extras"
 gn gen out/Debug --args="is_debug=true enable_dsyms=true symbol_level=2 $common_flags"
 gn gen out/Release --args="is_debug=false enable_dsyms=false symbol_level=0 $common_flags"
