e544aaee3b616fe0e4c158c4db9d30105859dc13
Dominic Létourneau <doumdi@gmail.com>
2022-02-16 16:32:56 -0500

    Building on Mac

--- opentera-webrtc-native-client/OpenteraWebrtcNativeClient/src/Utils/thread.cpp
+++ opentera-webrtc-native-client/OpenteraWebrtcNativeClient/src/Utils/thread.cpp
@@ -12,7 +12,7 @@ bool setThreadPriority(thread &thread, ThreadPriority priority)
 {
 #if defined(WIN32) || defined(_WIN32)
     return SetPriorityClass(thread.native_handle(), static_cast<DWORD>(priority)) != 0;
-#elif defined(UNIX) || defined(__unix__) || defined(LINUX) || defined(__linux__)
+#elif defined(UNIX) || defined(__unix__) || defined(LINUX) || defined(__linux__) || defined(__APPLE__)
     int topPriority = sched_get_priority_max(SCHED_RR) - 1;
     int lowPriority = sched_get_priority_min(SCHED_RR) + 1;
 
