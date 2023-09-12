7e3d4625ae304757998e1a76f1cf919fde8ac8bf
Marc-Antoine Maheux <Marc-Antoine.Maheux@USherbrooke.ca>
2023-09-12 16:59:12 -0400

    Revert "Fix H264 decoder on Jetpac 5.1.1."
    This reverts commit 62a2d6607090eb7995ce3c53639488a1c8e2495a.

--- opentera-webrtc-native-client/OpenteraWebrtcNativeGStreamer/src/Decoders/H264GStreamerVideoDecoders.cpp
+++ opentera-webrtc-native-client/OpenteraWebrtcNativeGStreamer/src/Decoders/H264GStreamerVideoDecoders.cpp
@@ -21,14 +21,8 @@
 using namespace opentera;
 using namespace std;
 
-H264GStreamerVideoDecoder::H264GStreamerVideoDecoder(
-    string decoderPipeline,
-    const char* mediaTypeCapsParameters,
-    bool resetPipelineOnSizeChanges)
-    : GStreamerVideoDecoder(
-          string(mediaTypeCaps()) + mediaTypeCapsParameters,
-          move(decoderPipeline),
-          resetPipelineOnSizeChanges)
+H264GStreamerVideoDecoder::H264GStreamerVideoDecoder(string decoderPipeline, bool resetPipelineOnSizeChanges)
+    : GStreamerVideoDecoder(mediaTypeCaps(), move(decoderPipeline), resetPipelineOnSizeChanges)
 {
 }
 
@@ -92,7 +86,7 @@ bool VaapiH264GStreamerVideoDecoder::isHardwareAccelerated()
 
 
 TegraH264GStreamerVideoDecoder::TegraH264GStreamerVideoDecoder()
-    : H264GStreamerVideoDecoder("nvv4l2decoder ! nvvidconv", ",alignment=au,stream-format=byte-stream", true)
+    : H264GStreamerVideoDecoder("h264parse ! nvv4l2decoder ! nvvidconv", true)
 {
 }
 
@@ -106,8 +100,8 @@ webrtc::VideoDecoder::DecoderInfo TegraH264GStreamerVideoDecoder::GetDecoderInfo
 
 bool TegraH264GStreamerVideoDecoder::isSupported()
 {
-    return gst::elementFactoryExists("x264enc") && gst::elementFactoryExists("nvv4l2decoder") &&
-           gst::elementFactoryExists("nvvidconv") &&
+    return gst::elementFactoryExists("x264enc") && gst::elementFactoryExists("h264parse") &&
+           gst::elementFactoryExists("nvv4l2decoder") && gst::elementFactoryExists("nvvidconv") &&
            gst::testEncoderDecoderPipeline("x264enc ! nvv4l2decoder ! nvvidconv");
 }
 
