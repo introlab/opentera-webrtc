c92aa741c1201193ac291a0a5d293606a9879c7d
mamaheux <35638081+mamaheux@users.noreply.github.com>
2023-09-13 08:36:49 -0400

    Fix h264 hardware decoding on jetpack 5.1.1 (#128)
    * Fix H264 decoder on Jetpac 5.1.1.
    
    * Revert "Fix H264 decoder on Jetpac 5.1.1."
    
    This reverts commit 32bb09c7e42fc55db1007fa46ea41f5f770ee320.
    
    * Fix H264 decoder on Jetpac 5.1.1.
    
    * Revert "Fix H264 decoder on Jetpac 5.1.1."
    
    This reverts commit c3a8666ad2ecfaa34bf55eb40476b217af1d0826.
    
    * Fix H264 decoder on Jetpac 5.1.1.
    
    * Revert "Fix H264 decoder on Jetpac 5.1.1."
    
    This reverts commit 62a2d6607090eb7995ce3c53639488a1c8e2495a.
    
    * Fix H264 decoder on Jetpac 5.1.1.
    
    * Fix H264 decoder on Jetpac 5.1.1.
    
    * Bump the version

--- opentera-webrtc-native-client/OpenteraWebrtcNativeGStreamer/src/Decoders/H264GStreamerVideoDecoders.cpp
+++ opentera-webrtc-native-client/OpenteraWebrtcNativeGStreamer/src/Decoders/H264GStreamerVideoDecoders.cpp
@@ -86,7 +86,7 @@ bool VaapiH264GStreamerVideoDecoder::isHardwareAccelerated()
 
 
 TegraH264GStreamerVideoDecoder::TegraH264GStreamerVideoDecoder()
-    : H264GStreamerVideoDecoder("h264parse ! nvv4l2decoder ! nvvidconv", true)
+    : H264GStreamerVideoDecoder("nvv4l2decoder ! nvvidconv", true)
 {
 }
 
@@ -100,8 +100,8 @@ webrtc::VideoDecoder::DecoderInfo TegraH264GStreamerVideoDecoder::GetDecoderInfo
 
 bool TegraH264GStreamerVideoDecoder::isSupported()
 {
-    return gst::elementFactoryExists("x264enc") && gst::elementFactoryExists("h264parse") &&
-           gst::elementFactoryExists("nvv4l2decoder") && gst::elementFactoryExists("nvvidconv") &&
+    return gst::elementFactoryExists("x264enc") && gst::elementFactoryExists("nvv4l2decoder") &&
+           gst::elementFactoryExists("nvvidconv") &&
            gst::testEncoderDecoderPipeline("x264enc ! nvv4l2decoder ! nvvidconv");
 }
 
